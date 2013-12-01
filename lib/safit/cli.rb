fb2631e24c296298937c77c0abc6cc0702e27d2c
Andrew Hartford <andrew.hartford@doxo.com>
2013-12-01 14:03:06 -0500

    Implement exec action

--- lib/safit/cli.rb
+++ lib/safit/cli.rb
@@ -6,5 +6,18 @@ module Safit
     def alert(message)
       Safit::Gntp.notify message
     end
+
+    desc "exec", "run command send alert"
+    def exec(*command)
+      cmd = command.join(' ')
+
+      Kernel.fork do
+        Process.exec cmd
+      end
+
+      Process.wait
+      Safit::Gntp.notify $?.success? ? "DONE: #{cmd}" : "FAIL: #{cmd}"
+      exit $?.exitstatus
+    end
   end
 end
